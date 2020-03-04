b09dc0af19d17ea53d42a2e2cb6164296f8dca99
Vasyl Borzianytsia <vasvas1988@list.ru>
2020-03-04 19:06:44 +0100

    For today done

--- app/form.php
+++ app/form.php
@@ -12,38 +12,28 @@ if(empty($_POST['email']) || empty($_POST['phone'])){
 //    header('../index.html?massage=emptyFields');
 }
 
-//$u_name = $_POST['name'];
-//$u_phone = $_POST['phone'];
-
 $u_address = 'Street: '. $_POST['street'] . 'Home: '.$_POST['home'] . 'A: '.$_POST['part'] . $_POST['appt'] . $_POST['floor'];
 $comment = $_POST['comment'];
-
-
 $payment = $_POST['payment'];
-//1=true,0=false to make it tinyint(1) in DB
-$callback = $_POST['callback'] === 1 ? 1 : 0;
+$callback = $_POST['callback'] === 1 ? 1 : 0; //1=true,0=false to make it tinyint(1) in DB
 
 //Get connection
 $instance = ConnectDb::getInstance();
 $conn = $instance->getConnection();
 
-
 //---STEP 1---//
 //Send query to DB to check if user exists
 try {
-    $rs = $conn->prepare('SELECT * FROM burger.users WHERE email = ?');
-    $rs->execute([$_POST['email']]);
-    $foo = $rs->fetchAll();
+    $foo = queryDb($conn,'SELECT * FROM burger.users WHERE email = ?',[$_POST['email']]);
+    print_r($foo);
 } catch (Exception $e) {
     die("Oh noes! There's an error in the query!");
 }
 
 if ( isset($foo) && empty($foo) ){
     try {
-        $rs = $conn->prepare('INSERT INTO burger.users (user_name, email, phone) values (?,?,?)');
-        $rs->execute([$_POST['name'],$_POST['email'],$_POST['phone']]);
-        $rs = $conn->prepare('INSERT INTO burger.orders (id_user, address, comment, payment, callback) values (?,?,?,?,?)');
-        $rs->execute([$foo[0]['id'],$u_address,$_POST['comment'],$payment,$callback]);
+        queryDb($conn,'INSERT INTO burger.users (user_name, email, phone) values (?, ?, ?)', [$_POST['name'], $_POST['email'], $_POST['phone']]);
+        queryDb($conn,'INSERT INTO burger.orders (id_user, address, comment, payment, callback) values (?,?,?,?,?)', [$foo[0]['id'], $u_address, $_POST['comment'], $payment, $callback]);
         session_start();
         $_SESSION['user'] = true;
     } catch (Exception $e) {
@@ -52,24 +42,11 @@ if ( isset($foo) && empty($foo) ){
 }else if( !empty($foo) ){
     session_start();
     $_SESSION['user'] = true;
-    try {
-        $rs = $conn->prepare('INSERT INTO burger.orders (id_user, address, comment, payment, callback) values (?,?,?,?,?)');
-        $rs->execute([$foo[0]['id'],$u_address,$_POST['comment'],$payment,$callback]);
-    } catch (Exception $e) {
-        die("Oh noes! There's an error in the query!");
-    }
+    queryDb($conn,'INSERT INTO burger.orders (id_user, address, comment, payment, callback) values (?,?,?,?,?)',[$foo[0]['id'],$u_address,$_POST['comment'],$payment,$callback]);
 
     //---STEP 3---//
-    try {
-        $rs = $conn->prepare('SELECT orders.id ,orders.address, orders.comment, orders.payment, orders.callback, users.phone, users.user_name, users.email FROM burger.users LEFT JOIN burger.orders ON users.id = orders.id_user where orders.id_user = ?');
-        $rs->execute([$foo[0]['id']]);
-        $foo = $rs->fetchAll();
-    } catch (Exception $e) {
-        die("Oh noes! There's an error in the query!");
-    }
-    echo '<pre>';
-    print_r($foo);
-    echo '</pre>';
+    $foo = queryDb($conn,'SELECT orders.id ,orders.address, orders.comment, orders.payment, orders.callback, users.phone, users.user_name, users.email FROM burger.users LEFT JOIN burger.orders ON users.id = orders.id_user where orders.id_user = ?',[$foo[0]['id']]);
+
     if (count($foo) === 1){
         echo 'This is your first order: ' . $foo['id'];
     }
@@ -78,22 +55,39 @@ if ( isset($foo) && empty($foo) ){
             'Order_Number' => "Заказ № " . $foo[sizeof($foo)-1]['id'] . PHP_EOL,
             'Address'      => 'Ваш заказ будет доставлен по адресу: '. $foo[sizeof($foo)-1]['address'] . PHP_EOL,
             'Product'      => 'DarkBeefBurger за 500 рублей, 1 шт.' . PHP_EOL,
-            'Comment'      => 'Спасибо! Это уже '. count($foo) . ' заказ' . PHP_EOL,
-            'Date'          => date('y-m-d',time()) . PHP_EOL
+            'Comment'      => 'Спасибо! Это уже '. count($foo) . ' заказ.' . PHP_EOL,
+            'Date'          => 'Date of order: ' . date('Y-m-d',time()) . PHP_EOL
         ];
         $file = '../orders/order' . $foo[sizeof($foo)-1]['id'] .'.txt';
+
         file_put_contents($file, $arr);
-        echo  'test';
     }
 //    header('../index.html?massage=successfully');
 }else{
     echo 'Something goes wrong, Please check your configuration';
 }
 
-
-
-
-//function insertDb($conn,array $arr, array $arr2,string $table) {
-//        $rs = $conn->prepare("'INSERT INTO $table ($arr) values (?,?,?,?,?)'");
-//        $rs->execute([$arr2]);
-//}
+function queryDb($conn,$sql,$bind) {
+    if(preg_match('/^INSERT/',$sql)){
+        echo '<pre>';
+        print_r(implode(', ',$bind));
+        echo '</pre>';
+        try {
+            $rs = $conn->prepare($sql);
+            $rs->execute([implode(', ',$bind)]);
+        }
+        catch (Exception $e) {
+            die("Oh noes! There's an error in the query!");
+        }
+    }
+    if(preg_match('/^SELECT/',$sql)){
+        try {
+            $rs = $conn->prepare($sql);
+            $rs->execute([implode(', ',$bind)]);
+            return $rs->fetchAll();
+        }
+        catch (Exception $e) {
+            die("Oh noes! There's an error in the query!");
+        }
+    }
+}
